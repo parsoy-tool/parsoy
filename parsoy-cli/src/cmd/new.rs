af244587517a5d8388a2afbb99cce888a8b21457
jer.clermont@gmail.com <jer.clermont@gmail.com>
2025-10-10 00:27:02 -0400

    update cli

--- parsoy-cli/src/cmd/new.rs
+++ parsoy-cli/src/cmd/new.rs
@@ -1,8 +1,21 @@
-use crate::ProjectTemplate;
+use std::path::PathBuf;
+
+use crate::{ProjectTemplate, handle_error};
 
 pub async fn run_new_command(
     project_name: String,
     template: ProjectTemplate,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    todo!()
+    let project_dir = PathBuf::from(&project_name);
+
+    std::fs::create_dir_all(&project_dir).unwrap();
+
+    match template {
+        ProjectTemplate::Compiler => run_new_compiler().await,
+        ProjectTemplate::Interpreter => run_new_interpreter().await,
+    }
 }
+
+async fn run_new_interpreter() -> Result<(), Box<dyn std::error::Error>> {}
+
+async fn run_new_compiler() -> Result<(), Box<dyn std::error::Error>> {}
